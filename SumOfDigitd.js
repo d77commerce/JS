cc54c95c3dee8029cbf8a493f3f73106b1002698
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2021-08-24 00:41:13 +0300

    Add files via upload

---
+++ SumOfDigitd.js
@@ -0,0 +1,9 @@
+function sumOfNumbers (input){
+  let num = input[0];
+  let total = 0;
+  for(let i = 0 ; i<num.length;i++){
+    total+= Number(num[i]);
+  }
+  console.log(`The sum of the digits is:${total}`);
+}
+sumOfNumbers(['332'])
