659fbaff3418fbe1df20e63c5ae830ba6638efb9
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-07 16:31:15 +0100

    repo

---
+++ funamentals 2/ageCalc/script.js
@@ -0,0 +1,9 @@
+function calcAge1(birthY){
+    return 2021-birthY;
+}
+const age = calcAge1(1977);
+const calcAge2 = function(birthY){
+    return 2021-birthY;
+}
+const age2 = calcAge2(2008);
+console.log(age, age2);
--- funamentals 2/race -/script.js
+++ funamentals 2/race -/script.js
@@ -6,7 +6,7 @@ const race1 = garage(3, 5);
 console.log(race1);
 
 const race2 = function garage2 (cars2, bikes2){
- return `cars race - ${cars2} and bikes race - ${bikes2}`
+ return `cars race - ${cars2} and bikes race - ${bikes2}`;
 }
 const result = race2(5, 9);
 console.log(result);
