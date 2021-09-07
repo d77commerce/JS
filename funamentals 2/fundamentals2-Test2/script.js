8ab0e4623787fe99f3357d98bae920e54a9fc7e8
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-07 15:51:54 +0100

    repo

---
+++ funamentals 2/fundamentals2-Test2/script.js
@@ -0,0 +1,6 @@
+function garage (cars, bikes){
+    const race = garage = `cars race - ${cars} and bikes race - ${bikes}`;
+    return race 
+}
+const race1 = garage(3, 5);
+console.log(race1)
---
+++ funamentals 2/fundamentals2-Test4/script.js
@@ -0,0 +1,13 @@
+function oneFun(){
+console.log('test 2 2');
+}
+oneFun();
+
+function secondFun (apple, orange){
+    const juice = `Juice of ${apple} apples and ${orange} oranges.`;
+    return juice;
+}
+    const appleJuice = secondFun(5, 0);
+    console.log(appleJuice);
+    const origeJuice = secondFun(1, 4);
+    console.log(origeJuice);
