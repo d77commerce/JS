b1c275e5c02eff902d6beb4f10284eff133711d3
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-10 23:18:07 +0100

    repo

---
+++ funamentals 2/fundamentals2-Test7/script.js
@@ -0,0 +1,10 @@
+function one(){
+    console.log('3 exercise - 5 repetition');
+for (let i = 1;i<4;i++){
+    console.log(`exercise${i}`);
+    for(let a = 1;a<=5;a++){
+        console.log(`repetition ${a}`);
+    }
+}
+}
+one();
