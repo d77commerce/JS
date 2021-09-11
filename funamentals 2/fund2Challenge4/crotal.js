e3d0f7471e937a12ab90240ab0ac8582418a6f50
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-11 01:48:32 +0100

    repo

---
+++ funamentals 2/fund2Challenge4/crotal.js
@@ -0,0 +1,23 @@
+function one (){
+    const bills=[22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+    let tips = [];
+    let totals =[];
+    let sum =0;
+    sumTips =0;
+    
+
+    
+    for(let i = 0; i<bills.length;i++){
+        let tip = bills[i]>=50 && bills[i]<=300 ? bills[i]*0.15:bills[i]*0.20;
+    tips.push(tip);  
+    totals.push(bills[i]+tip); 
+    sum+=(bills[i]+tip);
+    sumTips+=tip;
+    }
+   console.log(bills, tips, totals)
+   console.log(sum/10);
+   console.log(sumTips/10);
+}
+
+
+one();
